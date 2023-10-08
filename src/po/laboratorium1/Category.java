1a51cfad31a6378b965214308ee9da887e72de85
student <student@akademiakaliska.edu.pl>
2023-10-08 10:39:07 +0200

    lab1

---
+++ src/po/laboratorium1/Category.java
@@ -0,0 +1,32 @@
+package po.laboratorium1;
+
+public class Category {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        if (name == null || name.length()==0){
+            System.out.println("nieprawidłowa nazwa");
+        }else {
+            this.name = name;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Nazwa kategotii to: "+name;
+    }
+
+    public static void main(String[] args) {
+        Category kat = new Category();
+
+        kat.setName("Bartek");
+        System.out.println(kat.getName());
+        System.out.println(kat);
+        kat.setName(null);
+        kat.setName("");
+    }
+}
+
